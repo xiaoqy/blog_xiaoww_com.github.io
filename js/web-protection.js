c48033a65a0c35b9c4fc8a446854929a8fdf214e
unknown <ixiaoqy@gmail.com>
2025-07-10 13:59:39 +0800

    Site updated: 2025-07-10 13:58:59

---
+++ js/web-protection.js
@@ -0,0 +1,103 @@
+document.addEventListener('copy', function (event) {
+    let clipboardData = event.clipboardData || window.clipboardData;
+    if (!clipboardData) return;
+
+    let text = window.getSelection().toString();
+    if (text) {
+      event.preventDefault();
+
+      let copyrightNotice = '\r\n\r\n------\r\n作者：Allen Shaw\r\n链接：' + window.location.href + '\r\n未经授权禁止转载或摘录，著作权归作者所有！\r\n商业转载请联系作者获得许可，非商业转载请注明出处并保留作者信息。';
+      clipboardData.setData('text/plain', text + copyrightNotice);
+
+      // 如果想加富文本（HTML），可以加这一行
+      // clipboardData.setData('text/html', '<p>' + text + '</p><p>版权所有</p>');
+    }
+  });
+
+  // 禁止右键菜单
+  document.oncontextmenu = function(){alert("本站禁止右键菜单！"); return false; };
+  // 禁止文字选择
+  document.onselectstart = function(){return false; };
+  // 禁止复制
+  document.oncopy = function(){alert("本站禁止复制！"); return false; };
+  // 禁止剪切 
+  document.oncut = function(){alert("本站禁止剪切！"); return false; };
+  // 监听按键
+  document.onkeydown = function () {
+    // f12
+    if (window.event && window.event.keyCode == 123) {
+        event.keyCode = 0;
+        event.returnValue = false;
+
+        document.documentElement.innerHTML = '';
+        window.location.href = 'about:blank';
+        // document.write("<style>@media print{body{display:none}}</style>");
+        
+        // 初始状态
+        history.pushState(null, document.title, window.location.href);
+
+        // 监听popstate事件（用户点击了后退按钮）
+        window.addEventListener('popstate', function (event) {
+          history.pushState(null, document.title, window.location.href = 'about:blank');
+        });
+
+        alert("本站禁止调试！");
+    }
+    // ctrl+shift+i
+    if (window.event.ctrlKey && window.event.shiftKey && window.event.keyCode == 73) {
+        event.keyCode = 0;
+        event.returnValue = false;
+
+        document.documentElement.innerHTML = '';
+        window.location.href = 'about:blank';
+        // document.write("<style>@media print{body{display:none}}</style>");
+        
+        // 初始状态
+        history.pushState(null, document.title, window.location.href);
+
+        // 监听popstate事件（用户点击了后退按钮）
+        window.addEventListener('popstate', function (event) {
+          history.pushState(null, document.title, window.location.href = 'about:blank');
+        });
+
+        alert("本站禁止调试！");
+    }
+    // ctrl+s
+    if (window.event.ctrlKey && window.event.keyCode == 83) {
+        event.keyCode = 0;
+        event.returnValue = false;
+
+        document.documentElement.innerHTML = '';
+        window.location.href = 'about:blank';
+        // document.write("<style>@media print{body{display:none}}</style>");
+        
+        // 初始状态
+        history.pushState(null, document.title, window.location.href);
+
+        // 监听popstate事件（用户点击了后退按钮）
+        window.addEventListener('popstate', function (event) {
+          history.pushState(null, document.title, window.location.href = 'about:blank');
+        });
+
+        alert("本站禁止保存！");
+    }
+    // ctrl+p
+    if (window.event.ctrlKey && window.event.keyCode == 80) {
+        event.keyCode = 0;
+        event.returnValue = false;
+
+        document.documentElement.innerHTML = '';
+        window.location.href = 'about:blank';
+        // document.write("<style>@media print{body{display:none}}</style>");
+        
+        // 初始状态
+        history.pushState(null, document.title, window.location.href);
+
+        // 监听popstate事件（用户点击了后退按钮）
+        window.addEventListener('popstate', function (event) {
+          history.pushState(null, document.title, window.location.href = 'about:blank');
+        });
+        
+        alert("本站禁止打印！");
+    }
+  }
